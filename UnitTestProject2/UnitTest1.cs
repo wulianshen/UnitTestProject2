357c2a47175b045bc82749d58c72b81fa0400832
菁菁 李 <1821454283@qq.com>
2021-01-16 02:10:41 +0800

    添加项目文件。

---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private bool acceptNextAlert = true;
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            verificationErrors = new StringBuilder();
+        }
+
+        [TestCleanup]
+        public void CleanupTest()
+        {
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [TestMethod]
+        public void TheUntitledTestCaseTest()
+        {
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://www.baidu.com/");
+            //string title1 = driver.Title;
+            driver.FindElement(By.Id("kw")).Click();
+            driver.FindElement(By.Id("kw")).Clear();
+            driver.FindElement(By.Id("kw")).SendKeys("www");
+            driver.FindElement(By.Id("su")).Click();
+            string title2 = driver.Title;
+            Assert.AreEqual("百度一下，你就知道", title2);
+
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+
+    }
+}
